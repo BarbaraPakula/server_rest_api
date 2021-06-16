73897da3754dbc4c3e238bb5130d5f75e1e49eba
Barbara Pakula <barbara.pakula@hotmail.com>
2021-06-16 14:49:33 +0200

    deployment

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
-var cors = require('cors')
+var cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send({
@@ -21,6 +28,6 @@ app.use((req, res) => {
   });
 })
 
-app.listen(8000, () => {
-  console.log('Server listening on port: 8000')
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
